616f369f1ab0c4f7178cb3eb192ffc68f1219845
Eric Simmerman <ericsimmerman@gmail.com>
2017-04-30 23:04:28 -0400

    Rewriting for more explicit approach with less config assumption

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python
 
+# 27/04/2017 - forked by ets from :
+#
 # marek.kuczynski
 # @marekq
 # www.marek.rocks
 
-# set the bucketname where the security group log should be written to. If left blank, no log will be written. 
-bucketn     = 'lambda-firewall'  
+dynamicWhitelistDurationSeconds  = 60 * 60 * 24
+awsRegion = 'us-east-1'
+dynamicSGPrefix = 'dyn-'
+# We don't want to hardcode IDs here since those might change. We whitelist based upon the public IP
+# ElasticIPs attached to EC2 instances
+whiteListEC2Map = {"34.204.105.155": [22]}
+# ELB IPs
+whiteListELBMap = {"ELB_IP": [443]}
 
 ##### do not touch anything below this line #####
 
 import boto3, re, time
+import logging, sys
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-ec2id       = []
-ips         = []
-delgr       = []
-
-sgs         = {}
-sgdi        = {}
-timd        = {}
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
-def get_fw_rules(s, glist, prnt):
+# CRUFT!
+ids_ec2_instances_using_dynamicSG       = []
+
+securityGroups         = {}
+sgGroupidToRule        = {}
+stGroupIdToDescription        = {}
+
+
+def get_fw_rules(s, glist):
     resu            = []
     inst            = s.describe_instances()
 
@@ -28,205 +45,233 @@ def get_fw_rules(s, glist, prnt):
         for y in range(len(x[u'Instances'])):
             inst    = x[u'Instances'][int(y)][u'InstanceId']
             stat    = x[u'Instances'][int(y)][u'State'][u'Name']
-            ec2id.append(inst)
-            
+            ids_ec2_instances_using_dynamicSG.append(inst)
+
             if stat == 'running':
                 pubi        = x[u'Instances'][int(y)][u'PublicIpAddress']
-                if pubi not in ips:
-                    ips.append(pubi)
 
                 for z in range(len(x[u'Instances'][int(y)][u'NetworkInterfaces'])):
                     for a in range(len(x[u'Instances'][int(y)][u'NetworkInterfaces'][int(z)][u'Groups'])):
                         sggr        = x[u'Instances'][int(y)][u'NetworkInterfaces'][int(z)][u'Groups'][int(a)][u'GroupId']
-                 
-                        frpt        = sgdi[sggr][0]
-                        topt        = sgdi[sggr][1]
-                        cidr        = sgdi[sggr][2]
-                        grona       = sgdi[sggr][3]
-                        desc        = sgdi[sggr][4]
 
-                        if prnt == 'Y':
-                            a       = pubi, inst, desc, grona, sggr, str(topt), str(frpt), cidr
-                            b       = ' '.join(a)
-                            resu.append(str(b))
+                        frpt        = sgGroupidToRule[sggr][0]
+                        topt        = sgGroupidToRule[sggr][1]
+                        cidr        = sgGroupidToRule[sggr][2]
+                        groupName       = sgGroupidToRule[sggr][3]
+                        desc        = sgGroupidToRule[sggr][4]
 
                 for z in range(len(x[u'Instances'][int(y)][u'SecurityGroups'])):
                     groid       = x[u'Instances'][int(y)][u'SecurityGroups'][int(z)][u'GroupId']
-                    
-                    if sgs.has_key(inst):
-                        sgs[inst].append(groid)
+
+                    if securityGroups.has_key(inst):
+                        securityGroups[inst].append(groid)
                     else:
-                        sgs[inst] = [groid]
-    
+                        securityGroups[inst] = [groid]
+
     f       = open('/tmp/index.txt', 'w')
-    
+
     for x in resu:
         f.write(str(x).strip())
-        print 'RES: '+str(x).strip()
+        logger.info('RES: '+str(x).strip())
     f.close()
-    
+
     secg    = s.describe_security_groups()
     dele    = []
-    
-    for x in set(ec2id):
+
+    for x in set(ids_ec2_instances_using_dynamicSG):
         modi            = False
-        
+
         for y in secg[u'SecurityGroups']:
             groid       = y[u'GroupId']
             desc        = y[u'Description']
-            grona       = y[u'GroupName']
-          
-            if re.search('/32_', grona):
+            groupName       = y[u'GroupName']
+
+            if re.search('/32_', groupName):
                 unixt   = desc.split(' ')[3]
-            
+
                 if re.search(r"\b\d{10}\b", unixt):
                     z   = int(time.time()) - int(unixt)
-                        
+
                     if z > int(0):
-                        if groid in sgs[x]:
-                            sgs[x].remove(groid)
-                            
+                        if groid in securityGroups[x]:
+                            securityGroups[x].remove(groid)
+
                         if y[u'GroupName'] not in dele:
                             dele.append(y[u'GroupName'])
-                            
+
                         modi    = True
-            
-                        print 'DEL: removing '+groid+', '+grona+' from '+x+', age '+str(z/60)+' minutes, '+desc
+
+                        logger.info( 'DEL: removing '+groid+', '+groupName+' from '+x+', age '+str(z/60)+' minutes, '+desc)
                     else:
-                        print 'DEL: keeping '+groid+', '+grona+' from '+x+', age '+str(z/60)+' minutes, '+desc
+                        logger.info( 'DEL: keeping '+groid+', '+groupName+' from '+x+', age '+str(z/60)+' minutes, '+desc)
                 else:
-                    print 'DEL: keeping '+groid+', '+grona+' from '+x+', age '+str(z/60)+' minutes, '+desc
-        
+                    logger.info( 'DEL: keeping '+groid+', '+groupName+' from '+x+', age '+str(z/60)+' minutes, '+desc)
+
             else:
-                print 'DEL: keeping '+groid+', '+grona+' from '+x+' , '+desc
-        
+                logger.info( 'DEL: keeping '+groid+', '+groupName+' from '+x+' , '+desc)
+
         if modi:
-            s.modify_instance_attribute(Groups = sgs[x], InstanceId = x)
+            s.modify_instance_attribute(Groups = securityGroups[x], InstanceId = x)
 
     return dele, resu
-    
-        
-def get_session(serv):
-    s               = boto3.session.Session()
-    session         = s.client(serv, region_name = 'eu-west-1')
-    return session
-    
-
-def get_secgroups(s, prnt):
-    glist           = []
-    delgr           = []
-    sgs             = s.describe_security_groups()
-    
-    for sg in sgs['SecurityGroups']:
-        desc        = sg['Description']
-        grona       = sg['GroupName']
-        groid       = sg['GroupId']
-
-        if 'VpcId' in sg.keys():
-            vpcid   = sg['VpcId']
-        else:
-            vpcid   = ''
-
-        print sg['IpPermissions']
-
-        for ipp in sg['IpPermissions']:
-            frpt    = ipp['FromPort']
-            topt    = ipp['ToPort']
-
-            for ipr in ipp['IpRanges']:
-                cid = ipr['CidrIp']
-                z   = '"'+groid+'","'+str(frpt)+'","'+str(topt)+'","'+cid+'","'+desc+'","'+grona+'","'+vpcid+'"'
-
-            sgdi[groid]     = [frpt, topt, cid, grona, desc]
-            timd[groid]     = desc
-            
-            glist.append(cid+'/32_'+str(topt))
-
-            if prnt == 'Y':
-                print z+'\n'
-                
-    return glist
 
 
-def create_sg(s, cidr_ip, port, dura, proto, glist):
-    for iid in set(ec2id):
+
+
+def whitelist_ip(ec2Client, ipToWhitelist):
+    # For each public IP, find the ELB or EC2 instance using it
+    elbClient = get_bolo_client('elb')
+    loadbalancers = elbClient.describe_load_balancers()
+    for lb in loadbalancers:
+
+
+    ec2Instances = ec2Client.describe_instances(Filters=[
+        {
+            'Name': 'ip-address',
+            'Values': [
+                whiteListEC2Map.keys()
+            ]
+        },
+    ])
+    for instance in ec2Instances:
+        instanceId    = x[u'Instances'][int(y)][u'InstanceId']
+        s.modify_instance_attribute(Groups = a, InstanceId = instanceId)
+
+http://boto3.readthedocs.io/en/latest/reference/services/elb.html#ElasticLoadBalancing.Client.describe_load_balancers
+Remove API Gateway & lambda from Ohio  region
+
+    for x in ec2Instances[u'Reservations']:
+        for y in range(len(x[u'Instances'])):
+            inst    = x[u'Instances'][int(y)][u'InstanceId']
+            stat    = x[u'Instances'][int(y)][u'State'][u'Name']
+            ids_ec2_instances_using_dynamicSG.append(inst)
+
+            if stat == 'running':
+                pubi        = x[u'Instances'][int(y)][u'PublicIpAddress']
+
+                for z in range(len(x[u'Instances'][int(y)][u'NetworkInterfaces'])):
+                    for a in range(len(x[u'Instances'][int(y)][u'NetworkInterfaces'][int(z)][u'Groups'])):
+                        sggr        = x[u'Instances'][int(y)][u'NetworkInterfaces'][int(z)][u'Groups'][int(a)][u'GroupId']
+
+                        frpt        = sgGroupidToRule[sggr][0]
+                        topt        = sgGroupidToRule[sggr][1]
+                        cidr        = sgGroupidToRule[sggr][2]
+                        groupName       = sgGroupidToRule[sggr][3]
+                        desc        = sgGroupidToRule[sggr][4]
+
+                for z in range(len(x[u'Instances'][int(y)][u'SecurityGroups'])):
+                    groid       = x[u'Instances'][int(y)][u'SecurityGroups'][int(z)][u'GroupId']
+
+                    if securityGroups.has_key(inst):
+                        securityGroups[inst].append(groid)
+                    else:
+                        securityGroups[inst] = [groid]
+
+    # Create a SG with the appropriate ingress rule
+    # Attach the new SG to the instance
+
+
+    # Create the new SecurityGroup
+    creationTime      = int(time.time())
+    expireTime    = creationTime + int(dynamicWhitelistDurationSeconds)
+    desc = 'ExpiresAt ' + str(expireTime)
+    name = dynamicSGPrefix + 'ST-' +ipToWhitelist
+    try:
+        resp    = ec2Clientcreate_security_group(GroupName = name, Description = desc)
+        sgid    = resp.get(u'GroupId')
+        return sgid
+    except Exception as e:
+        logger.error( 'Unable to create SecuritGroup ['+name+']: '+str(e))
+
+    # Then attached the SG to the appropriate instance or ELB
+    s.modify_instance_attribute(Groups = securityGroups[x], InstanceId = x)
+
+
+
+
+    for iid in set(ids_ec2_instances_using_dynamicSG):
         name        = cidr_ip+'/32_' + str(port)
         modi        = False
 
         crea_u      = int(time.time())
         crea_t      = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(crea_u))
-        
+
         expire_u    = crea_u + int(dura)
         expire_t    = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(expire_u))
 
-        desc        = str(crea_u) + ' ' + str(crea_t) + ' ' + str(expire_u) + ' ' + str(expire_t) 
+        desc        = str(crea_u) + ' ' + str(crea_t) + ' ' + str(expire_u) + ' ' + str(expire_t)
 
         if name not in glist:
             try:
                 resp    = s.create_security_group(GroupName = name, Description = desc)
                 sgid    = resp.get(u'GroupId')
-            
+
                 s.authorize_security_group_ingress(GroupId = sgid, IpProtocol = proto, CidrIp = cidr_ip+'/32', FromPort = int(port), ToPort = int(port))
                 glist.append(name)
                 modi    = True
-                
+
             except Exception as e:
-                print 'ERROR: '+str(e)
+                logger.error( 'ERROR: '+str(e))
 
         else:
-            for k, v in sgdi.iteritems():
+            for k, v in sgGroupidToRule.iteritems():
                 if name     == v[2]+'_'+str(v[1]):
                     sgid    = k
 
-        a           = sgs[iid]
+        a           = securityGroups[iid]
 
         if modi:
             a.append(sgid)
             s.modify_instance_attribute(Groups = a, InstanceId = iid)
 
 
-def delete_sg(s, d):
-    for x in d:
-        try:
-            s.delete_security_group(GroupName = x)
-        
-        except Exception as e:
-            print 'FAIL: failed deleting ', str(x), e
+def get_bolo_client(serv):
+    s               = boto3.session.Session()
+    client         = s.client(serv, region_name = awsRegion )
+    return client
 
 
-def write_s3(s, filen, bucketn, k):
-    s.upload_file(filen, Bucket = bucketn, Key = k)
+def remove_expired_security_groups(client):
+    groupIds = []
+    securityGroups = client.describe_security_groups()
 
+    # Check all dynamic SecurityGroups and delete any that have expired
+    for sg in securityGroups['SecurityGroups']:
+        groupName = sg['GroupName']
+        if groupName.startswith( dynamicSGPrefix ):
+            groupDesc = sg['Description']
+            expireTime = groupDesc.split(' ')[1]
+            timeTillExpiration   = int(time.time()) - int(expireTime)
+            logger.info('timeTillExpiration= '+str(timeTillExpiration))
+            if timeTillExpiration > int(0):
+                try:
+                    logger.info( 'DRY RUN: would have deleted GroupId='+ str(groupId) )
+                    #s.delete_security_group(GroupId = groupId)
+                except Exception as e:
+                    logger.error( 'FAIL: failed deleting GroupId='+ str(GroupId).format( e))
 
-def handler(event, context):
-    s       = get_session('ec2')
-    g       = get_secgroups(s, 'N')
-    d, r    = get_fw_rules(s, g, 'N')
-    delete_sg(s, d)
+            groupIds.append(sg['GroupId'])
 
-    a       = event.get('ip', 'False')
-    if a != 'False':
-        t1      = str(event['duration'])
-        t2      = int(event['duration']) * 60
-        
-        i       = event['ip']
-        p       = event['port']
-        o       = event['proto']
-        
-        print 'add groups '+str(g)
-        create_sg(s, i, p, t2, o, g)
-    
-    g       = get_secgroups(s, 'Y')
-    d, r    = get_fw_rules(s, g, 'Y')
+    return groupIds
 
-    s       = get_session('s3')
-    k       = str(int(time.time()))+'.txt'
+def lambda_handler(event, context):
+    logger.info('got event{}'.format(event))
+    ec2Client = get_bolo_client('ec2')
+    dynGroupIds = remove_expired_security_groups(ec2Client)
 
-    write_s3(s, '/tmp/index.txt', bucketn, k)
-    write_s3(s, '/tmp/index.txt', bucketn, 'index.txt')
-    
-    if a != 'False':
-        return 'FIN: created security group for source IP '+i+' on port '+p+' for '+t1+' minutes to IP\'s '+', '.join(ips)
-    
-    else:  
+    isAnAccessRequest  = event.get('ip', 'False')
+    if isAnAccessRequest != 'False':
+        ipToWhitelist       = event['ip']
+        logger.info('Creating SG for '.format(ipToWhitelist))
+        whitelist_ip(ec2Client, ipToWhitelist)
+        result = 'FIN: created security groups to whitelist source IP '+ipToWhitelist+' for '+str(ruleDurationSeconds/60)+' minutes.'
+        logger.info(result)
+        return result
+    else:
         return r
+
+# CLI testing
+ec2Client = get_bolo_client('ec2')
+sgid = whitelist_ip(ec2Client,'173.73.153.176')
+groupIds = remove_expired_security_groups(ec2Client)
+logger.info(groupIds)
--- readme.md
+++ readme.md
@@ -1,22 +1,48 @@
 aws-lambda-firewall
 ===================
 
-Create temporary security groups on your EC2 instances through a simple API call. In addition, audit your security groups easily by the use of automated reports written to S3. 
+Fork of https://github.com/marekq/aws-lambda-firewall altered to support end users behind dynamic IPs who can "knock for access" using a valid API Gateway token. By making a valid call to this AWS Lambda function behind an AWS API Gateway, the end user's IP is added (for 24 hours) to a security group that permits access to other resources.
 
-
+IAM policies required by the role assigned to the lambda
+---------------------------------------------------------
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1392679134000",
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AuthorizeSecurityGroupEgress",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateSecurityGroup",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DescribeInstanceAttribute",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeInstances",
+        "ec2:DescribeNetworkAcls",
+        "ec2:DescribeSecurityGroups",
+        "ec2:RevokeSecurityGroupEgress",
+        "ec2:RevokeSecurityGroupIngress"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
 
 
 Description
 ------------
 
-The Lambda firewall can be used in sensitive environments where you want to keep strict control over security groups. Users with a valid API gateway key can make a request to whitelist a IP address for a specific duration without the need for access to the console. After the security group expires, it is automatically detached from the EC2 instances and removed. You no longer need to add or remove security groups manually, which is especially useful for users with many different  breakout IP addresses. 
+The Lambda firewall can be used in sensitive environments where you want to keep strict control over security groups. Users with a valid API gateway key can make a request to whitelist a IP address for a specific duration without the need for access to the console. After the security group expires, it is automatically detached from the EC2 instances and removed. You no longer need to add or remove security groups manually, which is especially useful for users with many different  breakout IP addresses.
 
-The steps how the Lambda firewall can be used are shown below; 
+The steps how the Lambda firewall can be used are shown below;
 
 ![alt tag](https://raw.githubusercontent.com/marekq/aws-lambda-firewall/master/docs/1.png)
 
 
-Besides security group management, the Lambda firewall will also write the state of security groups to an S3 folder so that it is possible to see the state of all security groups easily. This makes it very easy to review and check which ports are externally facing at any given time. 
+Besides security group management, the Lambda firewall will also write the state of security groups to an S3 folder so that it is possible to see the state of all security groups easily. This makes it very easy to review and check which ports are externally facing at any given time.
 
 
 
@@ -27,11 +53,11 @@ Installation
 You need to install two things in order for the firewall to work;
 
 1. Add the Lambda function to your account with handler "lambda_function.handler" and configure it with proper IAM permissions to run, see "lambda_function.py".
-2. Set the correct bucket name in the lambda function to write logs to S3 - you can skip this by entering a blank bucket name. 
+2. Set the correct bucket name in the lambda function to write logs to S3 - you can skip this by entering a blank bucket name.
 3. Create an API gateway and map the correct GET parameters to the Lambda function.
 4. Create API keys for users in the API gateweay and deploy the gateway to production.
-5. Next, create a trigger in CloudWatch so the Lambda function is called every 15 minutes to remove expired security groups. 
-6. Configure a valid API key and the correct Lambda URL in "firewall_client.py" and distribute it to your users. 
+5. Next, create a trigger in CloudWatch so the Lambda function is called every 15 minutes to remove expired security groups.
+6. Configure a valid API key and the correct Lambda URL in "firewall_client.py" and distribute it to your users.
 
 Make sure to use and enable CloudWatch logs if the Lambda function does not work.
 
@@ -41,8 +67,8 @@ Make sure to use and enable CloudWatch logs if the Lambda function does not work
 Usage
 -----
 
-- Security groups are added by the firewall_client which can be called manually by your users. 
-- Rules are removed when the function is called by the API gateway or when a valid API call is received. 
+- Security groups are added by the firewall_client which can be called manually by your users.
+- Rules are removed when the function is called by the API gateway or when a valid API call is received.
 
 
 If all steps are completed, users can whitelist an IP address and port simply by running "firewall_client.py" with the correct parameters. If these are skipped, default values are used;
@@ -90,4 +116,4 @@ Your API gateway method should look as follows;
 Contact
 -------
 
-For any questions or fixes, please reach out to @marekq! 
+For any questions or fixes, please reach out to @marekq!
